1612f08f47e1f06ba9ed4985f92391e67f439349
Astin Biju <astinbiju44@gmail.com>
2020-08-29 11:57:20 +0530

    Final-password text file added

--- final.py
+++ final.py
@@ -31,6 +31,74 @@ def lg_option(event):
             top.geometry('%dx%d+%d+%d' % (w, h, x, y))
             top.deiconify()
             root.withdraw()
+            filename = '/root/LocalDisk/Project/Python/Password_Viewer/mypassword'
+            file = open(filename, 'r')
+            account_name = []
+            account_name_dupli = []
+            fulladress_n = []
+            fulladress = []
+
+            my_frame = Frame(top)
+            my_scrollbar = Scrollbar(my_frame, orient=VERTICAL)
+
+            def roll(e, y, r):
+                pass_list.yview("scroll", 1, "units")
+                email_list.yview("scroll", 1, "units")
+
+            for i in file:
+                a = i.split("\n")
+                fulladress_n.append(a)
+
+            l = len(fulladress_n)
+
+            for i in range(0, l):
+                b = fulladress_n[i][0]
+                fulladress.append(b)
+
+            for i in range(0, l):
+                c = fulladress[i].split(",")
+                account_name.append(c[0])
+            account_name.sort()
+
+            [account_name_dupli.append(x) for x in account_name if x not in account_name_dupli]
+
+            def updateit():
+                return
+
+            def click(event):
+                global pass_list
+                global email_list
+                email_label = Label(top, text="Email:")
+                email_label.grid(row=0, column=1, padx=1, pady=10)
+                email_list = Listbox(top, height=1, width=30, yscrollcommand=my_scrollbar.set)
+                email_list.config(yscrollcommand=my_scrollbar.set)
+
+                email_list.grid(row=0, column=2, padx=(1, 1), pady=10)
+                pass_label = Label(top, text="Password:")
+                pass_label.grid(row=0, column=3, padx=(10, 0), pady=1)
+                pass_list = Listbox(top, height=1, width=30, yscrollcommand=my_scrollbar.set)
+                pass_list.grid(row=0, column=4, padx=1, pady=10)
+                my_scrollbar.config(command=roll)
+                my_scrollbar.grid(row=0, column=2)
+                my_frame.grid(row=0, column=2)
+                e = clicked.get()
+                for i in fulladress:
+                    f = i.split(",")
+                    if e in f:
+                        email_list.insert(END, f[1])
+                        pass_list.insert(END, f[2])
+
+            clicked = StringVar()
+            clicked.set("Click")
+            acc_drop = OptionMenu(top, clicked, *account_name_dupli, command=click)
+            acc_drop.grid(row=0, column=0, padx=10, pady=10)
+
+            updatebut = Button(top, text="Update", width=15, command=updateit)
+            updatebut.grid(row=1, column=0, padx=10, pady=5)
+
+            close_button = Button(top, text="Close", width=15, command=root.quit)
+            close_button.grid(row=2, column=0, padx=10, pady=5)
+
 
 
 
