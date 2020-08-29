24953d2c43e89cd95021661f47e5ed2e11b95641
Astin Biju <astinbiju44@gmail.com>
2020-08-29 11:35:31 +0530

    Login-Label and buttons added

--- login.py
+++ login.py
@@ -14,5 +14,26 @@ x = (ws/2) - (w/2)
 y = (hs/2) - (h/2)
 root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
+def lg_option(event):
+    return 
+
+
+
+
+
+lgpass_label=Label(root,text="Enter Your Password",font=("Helvetica", 16))
+lgpass_label.pack(pady=(10,0))
+
+lgpass_entry=Entry(root,width=50,show="*",bd=10,textvariable=10)
+lgpass_entry.bind("<Return>",lg_option)
+lgpass_entry.pack()
+
+
+lgbutton=Button(root,text="Continue",width=10,font=("Helvetica", 15),relief=GROOVE,command=lambda : lg_option("event"))
+lgbutton.bind("<Return>",lg_option)
+lgbutton.pack(pady=(10,0))
+
+
+
 
 root.mainloop()
